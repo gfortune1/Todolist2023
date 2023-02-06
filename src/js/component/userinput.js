24bbd5f0e1404d2138a170cc75acbbf685c1fa3e
gfortune1 <fgeee1@gmail.com>
2023-02-06 02:32:42 +0000

    to do list default

--- src/js/component/userinput.js
+++ src/js/component/userinput.js
@@ -1,10 +1,19 @@
 import React,{useState} from 'react'
 
 export default function Userinput() {
-    const [todoItem, setTodoItem]= useState("")
+    const [todoItem, setTodoItem]= useState("default")
   return (
-    <div><input onChange={(e)=>{
+    <div>
+        <input 
+        onChange={(e)=>{
         e.preventDefault()
-        console.log(e.target.value,"onChange e")}}type="text"/></div>
+            setTodoItem(e.target.value)
+        console.log(e.target.value,"onChange e")}}
+
+        type="text"/>
+        <div>
+            <p>user to do list : {todoItem}</p>
+        </div>
+    </div>
   )
 }
